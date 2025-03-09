eea308b0a5bfd559138cb326ce4d552bf918ba24
Vaibhav Sharma <138078161+vaibhav-sharma-dev@users.noreply.github.com>
2025-03-10 02:34:25 +0530

    feature: configured tailwind css

--- App.tsx
+++ App.tsx
@@ -133,6 +133,7 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import "./app.css"
 import Home from "./screens/Home";
 import Profile from "./screens/Profile";
 
